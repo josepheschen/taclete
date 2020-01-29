c61ed3452ecb9eb62152f5a98efafd9eb5b7ba61
josepheschen <jeschen@rams.colostate.edu>
2020-01-29 13:45:39 -0700

    onChange and onSubmit working for loginScreen.js

--- src/loginScreen.js
+++ src/loginScreen.js
@@ -4,20 +4,27 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        };
 
-    state = {
-        email: '',
-        password: '',
-    };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
     onChange = (e) => {
+        const value = e.target.value;
         this.setState({
-            [e.target.name]: e.target.value,
-        })
+            [e.target.name]: value
+        });
     };
-
+    o
     onSubmit = () => {
-        console.log("submitting");
+        console.log("Email: " + this.state.email);
+        console.log("Password: " + this.state.password);
     };
 
     useStyles = makeStyles(theme => ({
@@ -32,24 +39,26 @@ class LoginScreen extends Component {
     render() {
         return (
             <div>
-                <form className={this.useStyles.root} noValidate autoComplete="off">
+                <form className={this.useStyles.root} noValidate autoComplete="off" onChange={this.onChange}>
                     <TextField
-                        id="email-outlined"
+                        name="email"
                         variant="outlined"
                         label="Email"
                         type="text"
+                        value={this.state.email}
                         />
                     <p/>
                     <TextField
-                        id="password-input-outlined"
+                        name="password"
                         variant="outlined"
                         label="Password"
                         type="password"
                         autoComplete="current-password"
+                        value={this.state.password}
                         />
                 </form>
                 <p/>
-                <Button variant="contained"> Login </Button>
+                <Button variant="contained" onClick={this.onSubmit}> Login </Button>
             </div>
         );
     }
