feee8e8880c49c5a789807b43889c8e087dc14df
josepheschen <jeschen@rams.colostate.edu>
2020-01-28 21:45:57 -0700

    importing material-ui. implementing basic version of it

--- src/loginScreen.js
+++ src/loginScreen.js
@@ -1,43 +1,56 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
 class LoginScreen extends Component {
 
     state = {
         email: '',
         password: '',
-    }
+    };
 
     onChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value;
+            [e.target.name]: e.target.value,
         })
-    }
+    };
 
     onSubmit = () => {
         console.log("submitting");
-    }
+    };
+
+    useStyles = makeStyles(theme => ({
+        root: {
+            '& .MuiTextField-root': {
+                margin: theme.spacing(1),
+                width: 200,
+                color: 'white',
+            },
+        },
+    }));
 
     render() {
         return (
-            <div>
-                <h3 Login />
-                <Input
-                    name={'email'}
-                    placeHolder={'Email'}
-                    onChange={e => this.onChange(e)}
-                    value = this.state.email />
-                <Input
-                    name={'password'}
-                    placeHolder={'Password'}
-                    type={'password'}
-                    onChange={e => this.onChange(e)}
-                    value = this.state.email />
+            <form className={this.useStyles.root} noValidate autoComplete="off">
+                <TextField>
+                    id={'standard-basic'}
+                    defaultValue={'Email'}
 
+                    onChange={e => this.onChange(e)}
+                    value={this.state.email}
+                </TextField>
                 <br/>
+                <TextField>
+                    id={'standard-password-input'}
+                    defaultValue={'Password'}
+                    type={'password'}
 
-                <Button onClick={() => this.onSubmit()} type={"primary"} Go />
-
-            </div>
+                    onChange={e => this.onChange(e)}
+                    value={this.state.email}
+                </TextField>
+            </form>
         );
     }
 }
 export default LoginScreen;
+
