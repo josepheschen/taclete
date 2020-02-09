c78633b0f09a731a705f0357be6ad773a166cb07
CarsonEliasen <cceliasen@gmail.com>
2020-02-09 15:18:19 -0700

    Adds workout decriptions for coaches and athletes

--- src/CoachHome.js
+++ src/CoachHome.js
@@ -5,6 +5,7 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from "@material-ui/core/TextField";
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -16,18 +17,22 @@ class CoachHome extends Component {
     constructor(props, group) {
         super(props);
         this.state = {
-            Group: 'A',
+            Group: "A",
             setOpen: false,
             open: false,
             age: '',
             setAge: '',
+            workout: "Blank Workout",
         }
     }
+
+    getWorkout= () => {
+        return(this.state.workout)
+    };
 
     render() {
         return(
             <div>
-                <p>Coach View</p>
                 {this.state.Group}
                 {this.DialogSelect()}
             </div>
@@ -44,6 +49,12 @@ class CoachHome extends Component {
             minWidth: 120,
         },
     }));
+
+    workoutChange=(e)=>{
+        this.setState({
+            workout: e.target.value,
+        })
+    };
 
     DialogSelect() {
         const handleChange = event => {
@@ -65,48 +76,24 @@ class CoachHome extends Component {
 
         return (
             <div>
-                {this.Group}
-                <Button onClick={handleClickOpen}>New Workout</Button>
+                <h2>{this.Group} </h2>
+                <p> {this.state.workout}</p>
+                <Button onClick={handleClickOpen} color="#274f7a" variant="contained"
+                        style={{maxWidth: '150px', maxHeight: '50px', minWidth: '150px', minHeight: '50px'}}>
+                    New Workout
+                </Button>
                 <Dialog disableBackdropClick disableEscapeKeyDown open={this.state.open} onClose={handleClose}>
-                    <DialogTitle>Fill the form</DialogTitle>
+                    <DialogTitle>Make a new workout</DialogTitle>
                     <DialogContent>
-
-                        <form  style = {{minWidth: 240}}>
-                            <FormControl style = {{minWidth: 120}}>
-                                <InputLabel htmlFor="demo-dialog-native">Age</InputLabel>
-                                <Select
-                                    native
-                                    value={this.state.age}
-                                    onChange={handleChange}
-                                    input={<Input id="demo-dialog-native"/>}
-                                >
-                                    <option value=""/>
-                                    <option value={10}>Ten</option>
-                                    <option value={20}>Twenty</option>
-                                    <option value={30}>Thirty</option>
-                                </Select>
-                            </FormControl>
-
-                            <FormControl  style = {{minWidth: 120}}>
-                                <InputLabel id="demo-dialog-select-label">Age</InputLabel>
-                                <Select
-                                    labelId="demo-dialog-select-label"
-                                    id="demo-dialog-select"
-                                    value={this.state.age}
-                                    onChange={handleChange}
-                                    input={<Input/>}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-
-                        </form>
-
+                        <TextField
+                            id="outlined-multiline-static"
+                            label="Describe Workout"
+                            multiline
+                            rows="4"
+                            defaultValue={this.state.workout}
+                            variant="outlined"
+                            onChange={this.workoutChange}
+                        />
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={handleClose} color="primary">
