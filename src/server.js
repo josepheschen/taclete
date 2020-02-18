2f87ca16f0f44181b40328127b5c6c7b04b1d810
josepheschen <jeschen@rams.colostate.edu>
2020-02-17 21:32:11 -0700

    changed server.js, trying to figure out remote deployment

--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
 
-// viewed at http://localhost:8080
+
+app.use(express.static(publicPath));
 // app.use(express.static('../build'));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'index.html'));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(8080);
+app.listen(port, () => {
+    console.log(__dirname);
+    console.log(path.join(publicPath, 'index.html'));
+    console.log(`Server is up on port ${port}!`);
+});
+
+// viewed at http://localhost:8080
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// app.listen(8080);
